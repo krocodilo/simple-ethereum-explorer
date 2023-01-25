92518d51fd338d376f63927c82ae0269b11832ab
krocodilo <dcruz98.exe@gmail.com>
2023-01-25 23:54:10 +0000

    Turned Transaction class into a Record for simplicity

--- src/main/java/explorer/logic/models/Transaction.java
+++ src/main/java/explorer/logic/models/Transaction.java
@@ -1,25 +1,7 @@
 package explorer.logic.models;
 
-public class Transaction {
+public record Transaction(String blockNum, String hash, String timestamp,
+                          String from, String to, String ethValue,
+                          String gasUsed, String contractAddress) {
 
-    private String blocknum = "";
-    private String hash = "";
-    private String timestamp = "";
-    private String from = "";
-    private String to = "";
-    private String ethvalue = "";
-    private String gasused = "";
-    private String contractaddress = "";   // Used in transactions of contract creation
-
-
-    public Transaction(String blocknum, String hash, String timestamp, String from, String to, String ethvalue, String gasused, String contractaddress) {
-        this.blocknum = blocknum;
-        this.hash = hash;
-        this.timestamp = timestamp;
-        this.from = from;
-        this.to = to;
-        this.ethvalue = ethvalue;
-        this.gasused = gasused;
-        this.contractaddress = contractaddress;
-    }
 }
