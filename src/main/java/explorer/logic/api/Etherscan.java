5bc37a3342780b6abcace1d32bf80d81d16721ac
krocodilo <dcruz98.exe@gmail.com>
2023-01-30 21:41:42 +0000

    Small changes in the data saved locally from the data fetched from the etherscan API

--- src/main/java/explorer/logic/api/Etherscan.java
+++ src/main/java/explorer/logic/api/Etherscan.java
@@ -5,8 +5,11 @@ import explorer.logic.utils.HttpsConnection;
 import explorer.logic.utils.Utils;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.web3j.utils.Convert;
 
 import java.math.BigInteger;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.text.ParseException;
 import java.util.ArrayList;
 
@@ -70,29 +73,38 @@ public class Etherscan {
             for(Object obj : resp){
                 JSONObject j = (JSONObject) obj;
 
-                String m = j.getString("methodId");
+                String txnFee = new BigInteger(j.getString("gasUsed")).multiply(new BigInteger(j.getString("gasPrice"))).toString();
 
+                // Save JSON data to a transaction object
                 txns.add( new Transaction(
                         j.getString("blockNumber"),
                         j.getString("hash"),
-                        j.getString("timeStamp"),
+                        Utils.getDateFromUnixTime( Long.parseLong(j.getString("timeStamp")) ),
                         j.getString("from"),
-                        j.getString("to"),
-                        j.getString("value"),
-                        j.getString("gasUsed"),
-                        j.getString("contractAddress"),
-                        // Indicate if this transaction was an interaction with (or creation of) a contract:
-                        ! m.isBlank()
-                                && m.compareToIgnoreCase("0x") != 0
-                                && ! j.getString("contractAddress").isBlank()
+
+                        // If To field is empty, it signifies a contract was created:
+                        j.getString("to").isBlank() ? j.getString("contractAddress") : j.getString("to"),
+
+                        // Save value transfered in ETH, instead of Wei units:
+                        Convert.fromWei(j.getString("value"), Convert.Unit.ETHER).setScale(4, RoundingMode.HALF_EVEN).toPlainString(),
+
+                        // Save txn fee amount in ETH instead of Wei units:
+                        Convert.fromWei(txnFee, Convert.Unit.ETHER).setScale(4, RoundingMode.HALF_EVEN).toPlainString(),
+
+                        // Is a contract creation transaction if this field is not empty:
+                        ! j.getString("contractAddress").isBlank(),
+
+                        // Indicate if this transaction was an interaction with a contract:
+                        ! j.getString("functionName").isBlank(),
+
+                        Integer.parseInt(j.getString("isError")) != 0   // 0 = no error
                     ));
                 numTxnsReceived++;
             }
 
-            if(numTxnsReceived == 10000) {
-                // set the start block of the next request to the block of the last transaction processed
+            // set the start block of the next request to the block of the last transaction processed
+            if(numTxnsReceived == 10000)
                 firstBlock = new BigInteger( txns.get(txns.size()-1).blockNumber() );
-            }
 
             // Etherscan's API limit is 10k transactions per request
         } while(numTxnsReceived == 10000);
--- src/main/java/explorer/logic/models/Transaction.java
+++ src/main/java/explorer/logic/models/Transaction.java
@@ -1,8 +1,10 @@
 package explorer.logic.models;
 
 public record Transaction(String blockNumber, String hash, String timeStamp,
-                          String from, String to, String ethValueWei,
-                          String gasUsed, String contractAddress,
-                          boolean contractInteraction) {
+                          String from, String to, String ethValue,
+                          String txnFee,
+                          boolean contractCreated,
+                          boolean contractInteraction,  // Indicate if this transaction was an interaction with a contract
+                          boolean hasError) {
 
 }
